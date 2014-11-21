d71e0733a97f64db469c0edc1a70ce0b1148710e
william <willy.xiao1989@gmail.com>
2014-11-21 12:41:34 +0800

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 SmaileRemember::Application.routes.draw do
   resources :remember_items
   resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
