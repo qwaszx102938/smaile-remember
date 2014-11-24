426babbc7958f899a4dbf297d97511c021553ad5
willy.xiao <willy.xiao1989@gmail.com>
2014-11-24 14:00:09 +0800

    aaaaa

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ before_save { self.email = email.downcase }
 validates :username, presence: true
 validates :password, presence: true
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-validates :email, presence: true,unique: true,uniqueness: { case_sensitive: false },format:{with:VALID_EMAIL_REGEX}
+validates :email, presence: true,uniqueness: { case_sensitive: false },format:{with:VALID_EMAIL_REGEX}
 end
