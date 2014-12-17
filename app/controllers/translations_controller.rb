e9010657af51e744c7e6d23ada06bfbd20e3f3b7
willy.xiao <willy.xiao1989@gmail.com>
2014-12-18 00:19:52 +0800

    bbbb

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,31 +1,35 @@
 # http://fanyi.youdao.com/openapi.do?keyfrom=smiletalking&key=956262318&type=data&doctype=json&version=1.1&q=good
 class TranslationsController < ApplicationController
   def new
-
+    @translation=Translation.new
   end
 
   def create
-        trans_name=params[:translation][:trans].lstrip.rstrip
-		remember=Remember.find_by remember_content: trans_name
-		unless remember
-		    t_response=remote_trans trans_name
-			reference=t_response.parsed_response
-			remember=Remember.create(remember_content:trans_name,
-			remember_reference:reference.to_json)
-		end
-	
-    render text: remember.remember_reference
+    trans_name=params[:translation][:trans].lstrip.rstrip
+    if trans_name==""
+      render "create"
+    end
+    remember=Remember.find_by remember_content: trans_name
+    unless remember
+      t_response=remote_trans trans_name
+      reference=t_response.parsed_response
+      remember=Remember.create(remember_content: trans_name,
+                               remember_reference: reference.to_json)
+    end
+    @translation=Translation.new remember.remember_reference
+    #render text: @translation.trans_explains
   end
-  
-  private 
-  
+
+  private
+
   def remote_trans t_r
-      t_response=HTTParty.get 'http://fanyi.youdao.com/openapi.do',
-                 {query: {keyfrom: 'smiletalking', key: '956262318', type: 'data',
-                          doctype: 'json', version: '1.1',
-                          q: t_r}
-                 }
-		
+    t_response=HTTParty.get 'http://fanyi.youdao.com/openapi.do',
+                            {query: {keyfrom: 'smiletalking', key: '956262318', type: 'data',
+                                     doctype: 'json', version: '1.1',
+                                     q: t_r}
+                            }
+
   end
+
 end
 
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,51 +1,26 @@
-class Translation 
+#解析remember.remember_reference
+class Translation
 
 #trans_translation  翻译结果 数组
-#trans_translation  翻译结果 数组
-#trans_base    音标  hash
+#trans_explains    翻译解析  数组
 
-  attr_accessor :trans_name, :trans_translation,
-  
+  attr_accessor :trans_name, :trans_translation, :trans_explains
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-        BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  def initialize data_string=nil
+    if data_string
+      begin
+        data_hash=JSON.parse data_string
+        @trans_name=data_hash["query"]
+        @trans_translation=data_hash["translation"]
+        @trans_explains=data_hash["basic"]["explains"]
+      rescue
+        1
+      end
+    end
+    super()
   end
 
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-
-  #记住登录 本方法已自带update
-  def remember
-    self.remember_token=User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
-
-  def authenticated?(attribute, token)
-    digest=send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
-  end
-
-
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
-
-  def downcase_email
-    self.email = email.downcase
-  end
-
-  #创建一个激活码
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
-
-  def activate
-    user.update_attribute(:activated, true)
-    user.update_attribute(:activated_at, Time.zone.now)
+  def method_missing
+    nil
   end
 end
